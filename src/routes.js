bf6b3eef23e71a8c21135087b6061fe0a2dc7ec1
Matheus R da Cruz <matheus.cruzr@sonda.com>
2020-02-04 17:56:18 -0300

    continuando aplicacao

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import authMiddleare from './app/middlewares/auth';
 
 const routes = new Router();
+routes.post('/sessions', SessionController.store);
 
+routes.use(authMiddleare);
 routes.post('/users', UserController.store);
+routes.put('/users', authMiddleare, UserController.update);
+
 
 export default routes;
