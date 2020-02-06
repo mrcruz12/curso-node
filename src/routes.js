19b819d64c7089858678839f0e71cdfcf8ec8c84
Matheus R da Cruz <matheus.cruzr@sonda.com>
2020-02-06 17:53:34 -0300

    multer

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,22 @@
-import { Router } from 'express';
-import UserController from './app/controllers/UserController';
-import SessionController from './app/controllers/SessionController';
-import authMiddleare from './app/middlewares/auth';
+import { Router } from "express";
+import UserController from "./app/controllers/UserController";
+import SessionController from "./app/controllers/SessionController";
+import authMiddleare from "./app/middlewares/auth";
+import multer from "multer";
+import multerConfig from "./config/multer";
 
 const routes = new Router();
-routes.post('/sessions', SessionController.store);
 
-
-routes.post('/users', UserController.store);
+const update = multer(multerConfig);
+routes.post("/sessions", SessionController.store);
 routes.use(authMiddleare);
-routes.put('/users', authMiddleare, UserController.update);
 
+routes.post("/files", update.single("file"), (req, res) => {
+  return res.json({ ok: true });
+});
+
+routes.post("/users", UserController.store);
+
+routes.put("/users", authMiddleare, UserController.update);
 
 export default routes;
