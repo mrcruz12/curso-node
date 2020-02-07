a498f0b757aec6df9cf8742f237890d14f232406
Matheus R da Cruz <matheus.cruzr@sonda.com>
2020-02-07 16:52:29 -0300

    continuando

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+
 import authMiddleare from './app/middlewares/auth';
 import multerConfig from './config/multer';
 
@@ -15,5 +17,6 @@ routes.post('/users', UserController.store);
 routes.use(authMiddleare);
 routes.post('/files', update.single('file'), FileController.store);
 routes.put('/users', authMiddleare, UserController.update);
+routes.get('/providers', ProviderController.index);
 
 export default routes;
