be59891a0f355603a01ce992c6b4c08f4aa7833b
Matheus R da Cruz <matheus.cruzr@sonda.com>
2020-02-18 18:00:44 -0300

    mail config

--- src/lib/Mail.js
+++ src/lib/Mail.js
@@ -1,5 +1,5 @@
-import nodemailer from 'nodemailer';
-import mailConfig from '../config/mail';
+import nodemailer from "nodemailer";
+import mailConfig from "../config/mail";
 
 class Mail {
   constructor() {
@@ -9,14 +9,14 @@ class Mail {
       host,
       port,
       secure,
-      auth: auth.user ? auth : null,
+      auth: auth.user ? auth : null
     });
   }
-  sendMail(message){
-      return this.transporter.sendMail(){
-          ... mailConfig.default(),
-          ... message
-      }
+  sendMail(message) {
+    return this.transporter.sendMail({
+      ...mailConfig.default(),
+      ...message
+    });
   }
 }
 export default new Mail();
