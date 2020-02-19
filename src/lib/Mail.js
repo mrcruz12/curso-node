b1a864e735cc7046755f090ddc661976d0d32a65
Matheus Rodrigues da Cruz <matheus.rcruz12@gmail.com>
2020-02-18 21:19:04 -0300

    continuando

--- src/lib/Mail.js
+++ src/lib/Mail.js
@@ -1,21 +1,24 @@
-import nodemailer from "nodemailer";
-import mailConfig from "../config/mail";
+import nodemailer from 'nodemailer';
+import mailConfig from '../config/mail';
 
 class Mail {
   constructor() {
-    const { host, port, secure, auth } = mailConfig;
+    const {
+      host, port, secure, auth,
+    } = mailConfig;
 
     this.transporter = nodemailer.createTransport({
       host,
       port,
       secure,
-      auth: auth.user ? auth : null
+      auth: auth.user ? auth : null,
     });
   }
+
   sendMail(message) {
     return this.transporter.sendMail({
-      ...mailConfig.default(),
-      ...message
+      ...mailConfig.default,
+      ...message,
     });
   }
 }
